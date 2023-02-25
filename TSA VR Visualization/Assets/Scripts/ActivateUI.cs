efba8e779bf9a9c71caf5d6836490a8b50451bd2
Aryaman R <aryamanrtunjay@gmail.com>
2023-02-24 23:19:58 -0800

    UI Activation Prototype

--- TSA VR Visualization/Assets/Scripts/ActivateUI.cs
+++ TSA VR Visualization/Assets/Scripts/ActivateUI.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class ActivateUI : MonoBehaviour
 {
+    [SerializeField]
+    private Canvas UI;
+    [SerializeField]
+    private GameObject cube;
+    [SerializeField]
+    private Camera playerCam;
+    private int prevCollisions = 0;
+    private int CollisionsWithCube = 0;
+    private bool displayUI = false; 
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,24 @@ public class ActivateUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        DetectObjectWithRaycast();
+        UI.gameObject.SetActive(displayUI);
+    }
+    
+    private void DetectObjectWithRaycast()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = playerCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.name == "Cube")
+                {
+                    displayUI = !displayUI;
+                }
+            }
+        }
     }
 }
